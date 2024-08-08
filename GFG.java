8085ffba540645029b1f99fb9d30bf32cb1bfcaf
Arvind Lakra <arvindlakra50398@gmail.com>
2024-08-08 15:33:13 +0530

    yoo nigga i am still hera at 15:57

--- GFG.java
+++ GFG.java
@@ -2,11 +2,13 @@ public class GFG {
     // sw=software
     static String sw_name;
     static float sw_price;
+    static int  reg_no;
 
-    static void set(String n, float p)
+    static void set(String n, float p, int r)
     {
         sw_name = n;
         sw_price = p;
+        reg_no = r;
     }
 
     static void get()
@@ -14,11 +16,13 @@ public class GFG {
         System.out.println("Software name is: " + sw_name);
         System.out.println("Software price is: "
                 + sw_price);
+        System.out.println("Software registration no is: "
+                + reg_no);
     }
 
     public static void main(String args[])
     {
-        GFG.set("Visual studio", 0.0f);
+        GFG.set("Visual studio", 0.0f , 21);
         GFG.get();
     }
 }
